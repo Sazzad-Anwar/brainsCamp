a7aa09ca22bd1710d4ac3e93689e037ac2bde2bd
sazzad-anwar <sazzadzihan@gmail.com>
2022-03-06 15:43:17 +0600

    updated next.config and css link

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import "swiper/css";
 import "swiper/css/pagination"
 import "swiper/css/navigation"
 import "swiper/css/effect-fade"
-import "../styles/fonts.css"
 import "https://fonts.googleapis.com/icon?family=Material+Icons"
 import "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"
 import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
